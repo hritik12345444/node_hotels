f050f8f4136a981eef125fdcab9abbbcafeb6d06
Hritik singh <hritik12345444@gmail.com>
2026-05-24 21:46:19 +0530

    Add test log for addNumber function in notes.js
    Added a test log for the addNumber function.

--- notes.js
+++ notes.js
@@ -5,5 +5,5 @@ console.log("Notes are completed"); // 3rd me ye chalega
 
 
 console.log(serv.age);  // ye dusre file ka data hai usss file me jabb takk exports nhi krenge tbb tkk isme ussko use nahi krr skte hai 
-
+// for testing 
 console.log(serv.addNumber(serv.age,18));
